1c5927a7127d5f1f9fed033c2772407e66c77534
Tejas Ravi <tejasr9604@gmail.com>
2023-11-30 01:25:56 -0500

    Pokedex now shows all 1017 pokemon in numerical order and displays dex number + typing

--- app/src/main/java/com/example/pokedexapp/MainActivity.kt
+++ app/src/main/java/com/example/pokedexapp/MainActivity.kt
@@ -3,72 +3,95 @@ package com.example.pokedexapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
-    var pokemonImageURL = ""
-    var pokedexNumber = 0
-    var name = ""
-    var weight = ""
-    private lateinit var pokemonList: MutableList<String>
-    private lateinit var pokemonImageList: MutableList<String>
+    private lateinit var urlList: MutableList<String>
     private lateinit var nameList: MutableList<String>
-    private lateinit var weightList: MutableList<String>
+    private lateinit var imageList: MutableList<String>
+    private lateinit var typeList: MutableList<String>
     private lateinit var rvPokemon: RecyclerView
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        pokemonList = mutableListOf()
-        pokemonImageList = mutableListOf()
+        urlList = mutableListOf()
         nameList = mutableListOf()
-        weightList = mutableListOf()
+        imageList = mutableListOf()
+        typeList = mutableListOf()
         rvPokemon = findViewById(R.id.pokedexRecyclerView)
-        var pokemonAdapter = PokemonAdapter(pokemonImageList, nameList, weightList)
+
+        var pokemonAdapter = PokemonAdapter(urlList, nameList, typeList, imageList)
         rvPokemon.adapter = pokemonAdapter
         rvPokemon.layoutManager = LinearLayoutManager(this@MainActivity)
         val dividerItemDecoration = DividerItemDecoration(rvPokemon.context, LinearLayoutManager.VERTICAL)
         rvPokemon.addItemDecoration(dividerItemDecoration)
-        for (i in 0..49) {
-            pokedexNumber = (1..1017).random()
-            getPokemonInfo(pokedexNumber, object : PokemonInfoCallback {
-                override fun onSuccess(pokemonImageURL: String, name: String, weight: String) {
-                    pokemonImageList.add(pokemonImageURL)
-                    nameList.add(name)
-                    weightList.add(weight)
-                    pokemonAdapter.updateData(pokemonImageList, nameList, weightList)
-                    Log.d("please", weight)
-                    Log.d("finally0", name)
-                    Log.d("finally1",nameList.elementAt(nameList.size - 1))
-                    Log.d("finally2",pokemonImageURL)
-                    Log.d("finally3",pokedexNumber.toString())
-                }
-                override fun onFailure(error: String) {
-                }
-            })
-        }
+
+        getPokemonInfo(object : PokemonInfoCallback {
+            override fun onSuccess(urls: MutableList<String>, names: MutableList<String>, images: MutableList<String>, types: MutableList<String>) {
+                urlList = urls
+                nameList = names
+                imageList = images
+                typeList = types
+                pokemonAdapter.updateData(urlList, nameList, imageList, typeList)
+            }
+            override fun onFailure(error: String) {
+            }
+        })
     }
-    private fun getPokemonInfo(pokedexNumber: Int, callback: PokemonInfoCallback) {
-        val urlInput = "https://pokeapi.co/api/v2/pokemon/$pokedexNumber"
-        pokemonList.add(urlInput)
-        val client = AsyncHttpClient()
+        private fun getPokemonInfo(callback: PokemonInfoCallback) {
+            val urlInput = "https://pokeapi.co/api/v2/pokemon/?limit=1017"
+            val client = AsyncHttpClient()
+            client[urlInput, object : JsonHttpResponseHandler() {
+                override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                    val pokeJson = json.jsonObject.getJSONArray("results")
+                    val queue = LinkedList<Int>()
+                    for (i in 0 until pokeJson.length()) {
+                        queue.add(i)
+                    }
+                    processQueue(queue, client, callback)
+                }
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    errorResponse: String,
+                    throwable: Throwable?
+                ) {
+                    Log.d("Pokemon Error", errorResponse)
+                    callback.onFailure(errorResponse)
+                }
+            }]
+        }
+
+    private fun processQueue(queue: LinkedList<Int>, client: AsyncHttpClient, callback: PokemonInfoCallback) {
+        if (queue.isEmpty()) {
+            return
+        }
+        count++
+        val i = queue.removeFirst()
+        val urlInput = "https://pokeapi.co/api/v2/pokemon/${i+1}"
+        urlList.add(urlInput)
+        imageList.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png")
         client[urlInput, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                pokemonImageURL =
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokedexNumber.png"
-                name = json.jsonObject.getString("name").replaceFirstChar{it.uppercase()}
-                weight = json.jsonObject.getString("weight")
-                weight = (weight.toDouble() / 10).toString() + " kg"
-                Log.d("Pokemon", "response successful$json")
-                callback.onSuccess(pokemonImageURL, name, weight)
+                nameList.add(json.jsonObject.getString("name").replaceFirstChar{it.uppercase()})
+                val pokeJson = json.jsonObject
+                typeList.add(pokeJson.getJSONArray("types").getJSONObject(0).getJSONObject("type").getString("name"))
+                if (pokeJson.getJSONArray("types").length() > 1) {
+                    typeList[typeList.size-1] += "|" + pokeJson.getJSONArray("types").getJSONObject(1).getJSONObject("type").getString("name").replaceFirstChar{it.uppercase()}
+                }
+                if (count == 10) {
+                    callback.onSuccess(urlList, nameList, imageList, typeList)
+                    count = 0
+                }
+                processQueue(queue, client, callback)
             }
             override fun onFailure(
                 statusCode: Int,
@@ -81,8 +104,10 @@ class MainActivity : AppCompatActivity() {
             }
         }]
     }
-    interface PokemonInfoCallback {
-        fun onSuccess(pokemonImageURL: String, name: String, weight: String)
-        fun onFailure(error: String)
-    }
+
+}
+
+interface PokemonInfoCallback {
+    fun onSuccess(urls : MutableList<String>, names: MutableList<String>, images: MutableList<String>, types: MutableList<String>)
+    fun onFailure(error: String)
 }
--- app/src/main/java/com/example/pokedexapp/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedexapp/PokemonAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.pokedexapp
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +8,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class PokemonAdapter (private var pokeList: List<String>, private var nameList: List<String>, private var weightList: List<String>): RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+class PokemonAdapter (private var urlList : List<String>, private var nameList: List<String>, private var imageList: List<String>, private var typeList: List<String>): RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val pokemonImage: ImageView
         val pokemonText: TextView
-        val pokemonWeight: TextView
+        val pokedexNumber: TextView
+        val pokemonType: TextView
         init {
             pokemonImage = view.findViewById(R.id.pokemon_image)
             pokemonText = view.findViewById(R.id.pokemon_name)
-            pokemonWeight = view.findViewById(R.id.pokemon_weight)
+            pokedexNumber = view.findViewById(R.id.pokedex_number)
+            pokemonType = view.findViewById(R.id.pokemon_type)
         }
     }
 
@@ -28,22 +29,24 @@ class PokemonAdapter (private var pokeList: List<String>, private var nameList: 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.d("check","test")
         Glide.with(holder.itemView)
-            .load(pokeList[position])
+            .load(imageList[position])
             .fitCenter()
             .into(holder.pokemonImage)
-        holder.pokemonText.text = nameList[position]
-        holder.pokemonWeight.text = weightList[position]
+        holder.pokemonText.text = nameList[position].replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        val zeroes = "0".repeat(4 - (position+1).toString().length)
+        holder.pokedexNumber.text = "No." + zeroes +  (position+1).toString()
+        holder.pokemonType.text = typeList[position].replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
     }
 
     override fun getItemCount(): Int {
-        return pokeList.size
+        return imageList.size
     }
-    fun updateData(newPokemonImageList: MutableList<String>, newNameList: MutableList<String>, newWeightList: MutableList<String>) {
-        pokeList = newPokemonImageList
+    fun updateData(newURLList: MutableList<String>, newNameList: MutableList<String>, newImageList: MutableList<String>, newTypeList: MutableList<String>) {
+        urlList = newURLList
         nameList = newNameList
-        weightList = newWeightList
+        imageList = newImageList
+        typeList = newTypeList
         notifyDataSetChanged()
     }
 }
